e1b2f0671cdf5520aedcb4401bc3d9f9e85586e7
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-06-18 14:23:25 +0530

    import ethereumjs-Util ethjs just provides string manipulation. Use Ethereumjs-util

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const bip39 = require('bip39')
 const hdkey = require('./node_modules/hdkey/lib/hdkey.js')
 const mnemonic = bip39.generateMnemonic(); //generates string
-console.log(mnemonic);
+console.log("Mnemonic:",mnemonic);
 const seed = bip39.mnemonicToSeed(mnemonic); //creates seed buffer
-console.log(seed);
+// console.log(seed);
 const root = hdkey.fromMasterSeed(seed)
-console.log(root);
+// console.log(root);
 const masterPrivateKey = root.privateKey.toString('hex');
 console.log("Master private Key:",masterPrivateKey);
+
+const addrNode = root.derive("m/44'/60'/0'/0/0")
+const pubKey
