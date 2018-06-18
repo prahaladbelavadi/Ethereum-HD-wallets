934efc9b30b1f25bae54289c6ac18861c0ce8057
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-06-18 14:15:25 +0530

    gen priv key

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const bip39 = require('./node_modules/bip39/index.js')
-const HDkey = require('hdkey')
+const bip39 = require('bip39')
+const hdkey = require('./node_modules/hdkey/lib/hdkey.js')
 const mnemonic = bip39.generateMnemonic(); //generates string
 console.log(mnemonic);
 const seed = bip39.mnemonicToSeed(mnemonic); //creates seed buffer
@@ -7,4 +7,4 @@ console.log(seed);
 const root = hdkey.fromMasterSeed(seed)
 console.log(root);
 const masterPrivateKey = root.privateKey.toString('hex');
-console.log(masterPrivateKey);
+console.log("Master private Key:",masterPrivateKey);
