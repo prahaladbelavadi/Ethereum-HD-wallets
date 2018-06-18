3aa8bf3d5461cf3ea25e9e9c5aee27bbc3d37832
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-06-18 15:22:46 +0530

    generate address

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const bip39 = require('bip39')
 const hdkey = require('./node_modules/hdkey/lib/hdkey.js')
+const ethUtil = require('ethereumjs-util')
 const mnemonic = bip39.generateMnemonic(); //generates string
 console.log("Mnemonic:",mnemonic);
 const seed = bip39.mnemonicToSeed(mnemonic); //creates seed buffer
@@ -8,6 +9,12 @@ const root = hdkey.fromMasterSeed(seed)
 // console.log(root);
 const masterPrivateKey = root.privateKey.toString('hex');
 console.log("Master private Key:",masterPrivateKey);
-
-const addrNode = root.derive("m/44'/60'/0'/0/0")
-const pubKey
+var path = "m/44'/60'/0'/0/0";
+const addrNode = root.derive(path)
+console.log("path: ", path);
+const pubKey = ethUtil.privateToPublic(addrNode._privateKey)
+console.log("Pubkey as hex:",pubKey.toString('hex'));
+const addr = ethUtil.publicToAddress(pubKey).toString('hex');
+console.log("pubkey to Addr:",addr);
+const address = ethUtil.toChecksumAddress(addr)
+console.log("Address with Check sum:",address);
